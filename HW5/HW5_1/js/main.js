1d059fa607ab277440dca01f8058a5606a34a936
Vladimir <lenta-b@yandex.ru>
2016-11-21 23:49:41 +0300

    5_1

--- HW5/HW5_1/js/main.js
+++ HW5/HW5_1/js/main.js
@@ -1,29 +1,48 @@
 (function() {
 
-	var triggers = document.getElementsByClassName ("accordeon_trigger");
-		texts = document.getElementsByClassName ("accordeon_trigger-text");
-	
-	var showInner = function () {
-		var inner = this.nextElementSibling;
-			text = this.firstElementChild;
-		this.classList.toggle('orangecolor');
-		text.classList.toggle('whytetype');
-		if(inner !== null) {
-			inner.classList.toggle('active');
-		}
-		
-	};
+	var triggers  = document.getElementsByClassName("accordeon_trigger"),
+		texts     = document.getElementsByClassName("accordeon_trigger-text"),
+		accordeon = document.getElementsByClassName("accordeon");
 
-	for (var j = 1, leng = triggers.length; j < leng ; j++){
+	// Тут перекрашиваем промежуточные элементы аккардена в светлый цвет, 
+    // что бы в css не мучатся
+	
+	for (var j = 1, leng = triggers.length; j < leng ; j+=2){
 		triggers[j].classList.add('whiteback');
 		texts[j].classList.add('graytype');
-		j=j+1;
 	}
+	
+	// Добавляем прослушку на клик
 
+	document.addEventListener('click', rollDown);
+	
+	// Функция разворачивает вложеный элемент и укарашет согласно ТЗ
+	function rollDown (e) {
+		var target = e.target,
+			className = target.classList[0],
+			inner = target.nextElementSibling,
+			text = target.firstElementChild;
+		// Проверяем, туда ли именно мы нажали
 
-	for (var i = 0, len = triggers.length; i < len ; i++) {
-		triggers[i].addEventListener('click', showInner, false);
-	};
-
+		if (className == 'accordeon_trigger') {
+		
+			// Отменяем выдлеление при двойном клике, а то некарсиво
+			target.addEventListener('dblclick', noSelect);
+			function noSelect() {
+				target.onselectstart = function() {
+  				return false;
+				}
+			}
+			
+			// Навешиваем гирлянду классов 
+			
+			target.classList.toggle('orangecolor');
+			text.classList.toggle('whytetype');
+			if(inner !== null) {
+			inner.classList.toggle('active');
+			}
+		}
+	}
+	
 })(); 
 
