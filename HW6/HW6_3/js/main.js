3517736266b7bad6e5c6eef5cf89e10c3dc19af2
Vladimir <lenta-b@yandex.ru>
2016-11-24 01:21:38 +0300

    6_3

---
+++ HW6/HW6_3/js/main.js
@@ -0,0 +1,76 @@
+var p = new Promise (function(resolve, reject) {
+	
+	// Подключаемся и получаем JSON с городами
+	var xhr = new XMLHttpRequest();
+	xhr.responseType = "json";
+	xhr.open('GET','https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+	xhr.send();	
+	
+	// Если все ОК передаем ответ через resolve. 
+	// Если все плохо то reject
+	xhr.addEventListener('load',function(){
+		if(xhr.response) {
+			resolve(xhr.response);
+		} else {
+			reject();
+		}
+	});	
+});
+
+// Тут then которы у нас реагирует 
+p.then(function(obj) {
+	var cities = [];
+		var key = "";
+	// JSON переделываем в массив и все буквы делаем строчными.
+	// Поисковику не должно быть важно в каком регистре мы заносим информацию
+	for (let i = 0; i < obj.length; i++) {
+		cities[i] = obj[i].name.toLowerCase();
+	}
+		// Ставим прослушку на ввод данных в форму
+		input.addEventListener('keyup',function(event){
+			var kCode = event.key;
+			// Это отсекаем все служебные клавиши, кроме Backspace
+			if (kCode.length < 2) {
+				key += event.key.toLowerCase();
+				addElements();
+			} else if( kCode === 'Backspace')  {
+				key = key.slice(0, key.length-1);
+				addElements();
+			}
+			
+			
+			// Тут мы ищем совпадения в массиве
+			// И снова все города выводим с большой буквы
+			
+			function addElements() {
+				var list = document.createElement('ul'), 
+					c;
+				list.setAttribute('id','citylist');
+				
+				var listElem = document.getElementById('citylist');
+				
+				listElem ? wrap.removeChild(listElem):1;
+				wrap.appendChild(list);
+			
+				for(let i = 0; i < cities.length; i++) {
+					c = cities[i];
+					if (c.indexOf(key) + 1 && c && key) {
+						c = c[0].toUpperCase() + c.slice(1);
+						var elem = document.createElement('li');
+						elem.innerHTML = c;
+						citylist.appendChild(elem);
+					}
+				}
+			}
+		});
+	
+},
+function(){
+	console.log("список городов не подгрузился");
+});
+
+
+
+
+
+
